487fb5e03bd98b5eb53d627aaf4ce3bdc2b03d78
devalon <shimokawa@devalon.biz>
2022-05-27 11:07:08 +0900

    Try again

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://ichiwa.github.io/dist/",
   build: {
-    assetsDir: "./",
+    // assetsDir: "/",
   },
 });
