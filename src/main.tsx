1163b47c65e5f3041e28d74134aa48ad8c2284a9
ichiwa <shimokawa@devalon.biz>
2025-10-01 15:45:08 +0900

    Initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { HashRouter, Route, Routes } from "react-router-dom";
-import App from "./App";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </HashRouter>
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://devalon.biz/dist/",
+  base: "/",
   build: {
-    // assetsDir: "/",
+    outDir: "docs",
+    assetsDir: "assets",
   },
 });
